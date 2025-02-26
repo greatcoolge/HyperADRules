49ccf73758e9d09e9aaba7af62a3acb6a4be6e84
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-02-26 17:16:01 +0800

    domain_lists.sh

--- domain/domain_lists.sh
+++ domain/domain_lists.sh
@@ -4,10 +4,7 @@ set -e  # 在命令出错时立即退出脚本，并返回非零退出码
 set -x  # 输出每一条命令在执行时的状态
 
 # 定义要处理的 URL 列表
-urls="
-https://raw.githubusercontent.com/Cats-Team/AdRules/script/script/allowlist.txt
-https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt
-"
+urls="https://raw.githubusercontent.com/greatcoolge/neodevhost/refs/heads/master/allow"
 
 # 定义临时文件和输出文件的路径
 temp_file="all_domains.txt"  # 临时文件保存在当前目录
@@ -28,11 +25,14 @@ mkdir -p "$output_dir" || { echo "Failed to create directory $output_dir"; exit 
 # 下载、过滤并合并所有域名列表
 for url in $urls; do
   echo "Processing URL: $url"
-  curl -s "$url" | awk 'NF && !/^#|^!/' >> "$temp_file" || { echo "Failed to process $url"; exit 1; }
+  curl -s "$url" | awk 'NF && !/^#|^!/' | while read -r domain; do
+    # 在域名前加上 @@||，域名后加上 ^，并将结果追加到临时文件
+    echo "@@||$domain^" >> "$temp_file" || { echo "Failed to process $url"; exit 1; }
+  done
 done
 
-# 创建最终的 allow1.txt 文件，添加 @@||...^ 格式
-awk '{ print "@@||" $0 "^" }' "$temp_file" > "$output_file" || { echo "Failed to create $output_file"; exit 1; }
+# 将处理后的内容复制到输出文件
+mv "$temp_file" "$output_file" || { echo "Failed to create $output_file"; exit 1; }
 
 # 输出生成的文件路径
 echo "Generated file: $output_file"
