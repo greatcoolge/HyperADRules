23fadc379c286a6a3a1018dd5df986c31510af8b
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-02-26 15:09:04 +0800

    update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -34,6 +34,42 @@ rules=(
   "https://filters.adtidy.org/android/filters/3_optimized.txt"
   "https://filters.adtidy.org/android/filters/224_optimized.txt"
   "https://perflyst.github.io/PiHoleBlocklist/SmartTV-AGH.txt"
+#!/bin/sh
+LC_ALL='C'
+
+# 清理临时文件
+rm *.txt
+
+wait
+echo '创建临时文件夹'
+mkdir -p ./tmp/
+
+# 添加补充规则
+cp ./data/rules/adblock.txt ./tmp/rules01.txt
+cp ./data/rules/whitelist.txt ./tmp/allow01.txt
+
+cd tmp
+
+# 下载规则并处理
+# 下载 yhosts 规则
+curl -v https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts | sed '/0.0.0.0 /!d; /#/d; s/0.0.0.0 /||/; s/$/\^/' > rules001.txt
+
+# 下载大圣净化规则
+curl -v https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts > rules002.txt
+sed -i '/视频/d;/奇艺/d;/微信/d;/localhost/d' rules002.txt
+sed -i '/127.0.0.1 /!d; s/127\.0\.0\.1 /||/; s/$/\^/' rules002.txt
+
+# 下载乘风视频过滤规则
+curl -v https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt | awk '!/^$/{if($0 !~ /[#^|\/\*\]\[\!]/){print "||"$0"^"} else if($0 ~ /[#\$|@]/){print $0}}' | sort -u > rules003.txt
+
+echo '下载规则'
+rules=(
+  "https://filters.adtidy.org/android/filters/2_optimized.txt"
+  "https://filters.adtidy.org/android/filters/11_optimized.txt"
+  "https://filters.adtidy.org/android/filters/17_optimized.txt"
+  "https://filters.adtidy.org/android/filters/3_optimized.txt"
+  "https://filters.adtidy.org/android/filters/224_optimized.txt"
+  "https://perflyst.github.io/PiHoleBlocklist/SmartTV-AGH.txt"
   "https://easylist-downloads.adblockplus.org/easyprivacy.txt"
   "https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt"
   "https://raw.githubusercontent.com/d3ward/toolz/master/src/d3host.adblock"
@@ -62,8 +98,9 @@ allow=(
 # 并行下载规则文件
 for i in "${!rules[@]}" "${!allow[@]}"
 do
-  curl -m 60 --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "rules${i}.txt" --connect-timeout 60 -s "${rules[$i]}" | iconv -t utf-8 &
-  curl -m 60 --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "allow${i}.txt" --connect-timeout 60 -s "${allow[$i]}" | iconv -t utf-8 &
+  # 通过 tee 命令将日志输出到终端并同时写入文件
+  curl -v --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "rules${i}.txt" --connect-timeout 60 -s "${rules[$i]}" | tee -a download_log.txt &
+  curl -v --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "allow${i}.txt" --connect-timeout 60 -s "${allow[$i]}" | tee -a download_log.txt &
 done
 wait
 echo '规则下载完成'
