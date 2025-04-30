6d49dcb5dd18427d9f807f4c2cc2568264670a30
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-04-30 18:26:17 +0800

    aallowlist.sh

--- domain/aallowlist.sh
+++ domain/aallowlist.sh
@@ -91,34 +91,6 @@ cat allow*.txt | grep -v '#' | sed '/^$/d' \
 | sort -n | uniq | awk '!a[$0]++' > tmp-allow.txt
 wait
 
-# 最终合并文件
-echo '合并并去重规则和允许列表'
-
-# 合并允许列表和规则列表
-cat tmp-allow.txt tmp-rules.txt | \
-  # 过滤掉空行和注释行
-  grep -v -E '^(\s*$|#)' | \
-  # 去除无效的规则（如包含特殊字符或 IP 地址）
-  grep -Ev '#|!|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | \
-  # 确保规则格式一致，去掉额外的空格
-  sed 's/^[ \t]*//;s/[ \t]*$//' | \
-  # 排序并去重
-  sort -u > final-rules.txt
-
-echo '更新成功'
-
-# 运行Python处理后续
-python .././data/python/rule.py
-
-echo '更新成功'
-exit
-
-wait
-
-cat allow*.txt | grep -v '#' | sed '/^$/d' \
-| grep -v '!' | grep -P -v '[\x80-\xFF]' \
-| sort -n | uniq | awk '!a[$0]++' > tmp-allow.txt
-wait
 
 # 最终合并文件
 echo '合并并去重规则和允许列表'
