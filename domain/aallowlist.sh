55c976fa42386ccab110954827b8e7b5a162a645
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-04-30 14:35:30 +0800

    aallowlist.sh

--- domain/aallowlist.sh
+++ domain/aallowlist.sh
@@ -68,13 +68,25 @@ cat rules*.txt | sort -n | grep -v -E "^((#.*)|(\s*))$" \
  | sort \
  | uniq > base-src-domain.txt
 wait
+# 先处理 base-src-domain.txt 文件并生成 tmp-rules.txt
 cat base-src-domain.txt | grep -Ev '#|\$|@|!|/|\\|\*' \
- | grep -v -E "^((#.*)|(\s*))$" \
- | grep -v -E "^[0-9f\.:]+\s+(ip6\-)|(localhost|loopback)$" \
- | sed "s/^/@@||&/g" | sed "s/$/&^/g" | sed '/^$/d' \
- | grep -v '^#' \
- | sort -n | uniq | awk '!a[$0]++' \
- | grep -E "^((\|\|)\S+\^)" > tmp-rules.txt
+  | grep -v -E "^((#.*)|(\s*))$" \
+  | grep -v -E "^[0-9f\.:]+\s+(ip6\-)|(localhost|loopback)$" \
+  | sed "s/^/@@||&/g" | sed "s/$/&^/g" | sed '/^$/d' \
+  | grep -v '^#' \
+  | sort -n | uniq | awk '!a[$0]++' \
+  | grep -E "^((\|\|)\S+\^)" > tmp-rules.txt
+
+# 确认 tmp-rules.txt 的内容
+echo 'tmp-rules.txt内容：'
+cat tmp-rules.txt
+
+# 另外处理 tmp.txt
+cat tmp.txt | sed "s/^/@@||&/g" > tmp1.txt
+
+# 打印 tmp1.txt 的内容
+echo 'tmp1.txt内容：'
+cat tmp1.txt
 wait
 
 cat allow*.txt | grep -v '#' | sed '/^$/d' \
