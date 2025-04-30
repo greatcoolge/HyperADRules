54daf7161f7b393aacdd5c766d3f51feeebf830c
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-04-30 13:08:35 +0800

    aallowlist.sh

--- domain/aallowlist.sh
+++ domain/aallowlist.sh
@@ -6,6 +6,9 @@ wait
 echo '创建临时文件夹'
 mkdir -p ./tmp/
 
+#添加补充规则
+cp ./data/rules/adblock.txt ./tmp/rules01.txt
+cp ./data/rules/whitelist.txt ./tmp/allow01.txt
 # 下载规则列表
 echo '下载规则'
 rules=(
@@ -64,16 +67,28 @@ cat base-src-domain.txt | grep -Ev '#|\$|@|!|/|\\|\*' \
 wait
 
 cat allow*.txt | grep -v '#' | sed '/^$/d' \
- | sed "s/^/@@||&/g" | sed "s/$/&^/g" \
- | sort -n | uniq | awk '!a[$0]++' > tmp-allow.txt
+| grep -v '!' | grep -P -v '[\x80-\xFF]' \
+| sort -n | uniq | awk '!a[$0]++' > tmp-allow.txt
 wait
 
 # 最终合并文件
-cat tmp-allow.txt tmp-rules.txt > final-rules.txt
+echo '合并并去重规则和允许列表'
+
+# 合并允许列表和规则列表
+cat tmp-allow.txt tmp-rules.txt | \
+  # 过滤掉空行和注释行
+  grep -v -E '^(\s*$|#)' | \
+  # 去除无效的规则（如包含特殊字符或 IP 地址）
+  grep -Ev '#|\$|@|!|/|\\|\*|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | \
+  # 确保规则格式一致，去掉额外的空格
+  sed 's/^[ \t]*//;s/[ \t]*$//' | \
+  # 排序并去重
+  sort -u > final-rules.txt
+
+echo '更新成功'
 
 # 运行Python处理后续
 python .././data/python/rule.py
-python .././data/python/filter-dns.py
 python .././data/python/title.py
 
 echo '更新成功'
