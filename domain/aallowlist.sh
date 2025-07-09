69a81367b5b6e26bdba2d7881727617e6561bcc9
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-07-09 19:48:56 +0800

    aallowlist.sh

--- domain/aallowlist.sh
+++ domain/aallowlist.sh
@@ -27,7 +27,6 @@ allow=(
   "https://raw.githubusercontent.com/Kuroba-Sayuki/FuLing-AdRules/Master/FuLingRules/FuLingAllowList.txt"
   "https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/ok.txt"
   "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/whitelist-urlshortener.txt"
-  "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nsfw.txt"
   "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/whitelist-referral-native.txt"
   "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt"
   "https://raw.githubusercontent.com/ChengJi-e/AFDNS/master/QD.txt"
